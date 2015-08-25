1386ff09cb269c6dcbdedec0462df1376e5fdbcf
Sam Wenke <wenkesj@users.noreply.github.com>
2015-08-25 16:39:16 -0400

    Fix example require

--- server.js
+++ server.js
@@ -3,19 +3,22 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var server = require('http').createServer(app);
-var geolocation = require('./geolocation');
+var geocore = require('./geo-core');
 
 server.listen(config.port, function () {
     console.log('Server listening at port: ', config.port);
 });
 
+/**
+ * Allow CO
+ */
 app.all('/geo-core', function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	next();
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
 });
 
 app.get('/geo-core', function (req, res) {
-	geolocation.findNearbyLocations(req.query, function(nearby) {
+    geocore.findNearbyLocations(req.query, function(nearby) {
         res.send(nearby);
-	});
+    });
 });
